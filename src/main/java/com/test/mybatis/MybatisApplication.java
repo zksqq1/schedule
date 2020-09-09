7f7609bf2d22b15c345b8ad6498fac8ad66a3d6d
lin <617807893@qq.com>
2020-09-09 11:45:38 +0800

    test

--- src/main/java/com/test/mybatis/MybatisApplication.java
+++ src/main/java/com/test/mybatis/MybatisApplication.java
@@ -10,7 +10,6 @@ public class MybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
-        System.out.println("start");
     }
 
 }
