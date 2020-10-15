42903e6fa2533628544da3768007c34bd7393d1a
lin <617807893@qq.com>
2020-10-15 10:39:03 +0800

    test

--- src/main/java/com/test/mybatis/MybatisApplication.java
+++ src/main/java/com/test/mybatis/MybatisApplication.java
@@ -4,16 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableScheduling
 @MapperScan(basePackages = "com.test.mybatis.mapper")
 public class MybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
     }
-
 }
